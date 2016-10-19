b18344f302b0fba89efb7682a14f5b3526baca08
Desmond Lee <desmondclee@gmail.com>
2016-10-19 03:49:35 -0500

    almost working, except api gateway

---
+++ aws-cloudformation/compile.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+
+var resources = Object.assign(
+    {},
+    JSON.parse(fs.readFileSync('resources/fdbucket.json')),
+    JSON.parse(fs.readFileSync('resources/fdlambda.json')),
+    JSON.parse(fs.readFileSync('resources/restapi.json')),
+    JSON.parse(fs.readFileSync('security/policies.json')),
+    JSON.parse(fs.readFileSync('security/roles.json'))
+)
+
+var output = JSON.stringify({
+    "Resources": resources,
+    "Parameters": JSON.parse(fs.readFileSync('parameters.json')),
+    "Outputs": JSON.parse(fs.readFileSync('outputs.json'))
+}, null, 2)
+
+fs.writeFileSync('template.json', output)
